ec35d6c72a3b34bbaf14426e018247892051c4d3
Nayan Barhate <180070037@iitb.ac.in>
2020-06-29 16:23:36 +0530

    gradient

--- src/app/second-component/second-component.component.ts
+++ src/app/second-component/second-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';// import Jquery here
+
 import {
   trigger,
   state,
@@ -37,18 +38,18 @@ export class SecondComponentComponent implements OnInit {
 export class SecondComponentComponent {
   title = 'Meet our Team!';
   tiles: Tile[] = [
-      {text: 'Anjali', cols: 2, rows: 2, color: 'lightblue', 
+      {text: 'Anjali', cols: 2, rows: 2, color: ' linear-gradient( #ffd48f,#fa9600)', 
       content: 'Fill in your Description!', photoId: '',email:'',git:''},
 
-      {text: 'Avyakta', cols: 2, rows: 2, color: 'lightgreen', 
+      {text: 'Avyakta', cols: 2, rows: 2, color: 'linear-gradient( #e8ab8c,#fa4f00)', 
       content: `Hey There! I am Avyakta from Electrical Engineering Department of IIT-Bombay.`, 
       photoId:'https://drive.google.com/thumbnail?id=1WUdk_GejPk76rPJSU_mEbZp38CDUxYxF',
       email:"mailto:avyaktawrat@gmail.com",git:"https://www.github.com/avyaktawrat"},
 
-      {text: 'Ipsit', cols: 2, rows: 2, color: 'lightpink', 
+      {text: 'Ipsit', cols: 2, rows: 2, color: 'linear-gradient( #84bcf1,#5299eb)', 
       content: 'Fill in your Description!',photoId:'m',email:'',git:''},
 
-      {text: 'Nayan', cols: 2, rows: 2, color: '#DDBDF1', 
+      {text: 'Nayan', cols: 2, rows: 2, color: 'linear-gradient( #8fadff,#0047fa)', 
       content: 'Hey There! I am Nayan from Electrical Engineering Department of IIT-Bombay.',
       photoId:'https://drive.google.com/thumbnail?id=1-D26ZbQlj8eYTEHLCEEU_Ng5dTy4yLeV',
       email:"mailto:nbarhate151@gmail.com",git:"https://www.github.com/nayan0037"},
