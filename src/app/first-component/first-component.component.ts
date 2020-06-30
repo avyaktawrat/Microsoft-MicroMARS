de842be9590969629caacaea612866d7feb18ecf
avyaktawrat <avyaktawrat@gmail.com>
2020-06-30 13:22:44 +0530

    Adjusting width and defining double clicks

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { GridCoords } from './GridCoords';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-first-component',
@@ -10,8 +11,8 @@ export class FirstComponentComponent implements OnInit {
 
   constructor() {
   }
-  height = 653;
-  width = 1535;
+  height = window.innerHeight;
+  width = screen.height;
   gridCord: GridCoords[] = new Array(4200);
   ngOnInit() {
     for (let i = 0; i < 70; i++) {
@@ -24,7 +25,19 @@ export class FirstComponentComponent implements OnInit {
   changeColor(a: number, b: number): void {
     console.log(a,b);
     let element = document.getElementsByTagName('rect')[a+Math.floor(b/30)];
-    element.style.fill = 'red';
+    if (element.style.fill == 'red'){
+      element.style.fill = 'white';
+    }
+    else {element.style.fill = 'red'}
+  }
+  //choose obstacle
+  chooseColor(a: number, b: number): void {
+    console.log(a,b);
+    let element = document.getElementsByTagName('rect')[a+Math.floor(b/30)];
+    //window.alert("Hello There");
+    //let value:number = window.prompt("Please enter obstacle %");
+    //console.log(value);
+    //element.style.fill = "rgb(256, 0, 0,value/100)";
   }
 
 }
