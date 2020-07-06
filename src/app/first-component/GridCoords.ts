509bc5086918162b59193cd33f3f7408ceffe40e
Nayan Barhate <180070037@iitb.ac.in>
2020-07-07 04:37:13 +0530

    grid adjustable

--- src/app/first-component/GridCoords.ts
+++ src/app/first-component/GridCoords.ts
@@ -2,7 +2,4 @@ export interface GridCoords {
     x: number;
     y: number;
     obstacle:number; // reward matrix
-    f: number;
-    g: number;
-    h: number;
   }
--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -13,10 +13,11 @@ export class FirstComponentComponent implements OnInit {
   } 
   height = window.innerHeight;
   width = screen.width;
-  gridCord: GridCoords[] = new Array(25);
-  hGrid :number = 5;
-  vGrid :number = 5;
+
+  hGrid :number = Math.floor((this.height-100)/30);
+  vGrid :number = Math.floor((this.width-120)/30);
   totalGrid : number =this.hGrid * this.vGrid;
+  gridCord: GridCoords[] = new Array(this.totalGrid);
   mouseDown : boolean = false;
   toFill:boolean = true;
   color :number = 2; //0 red 1 green 2 other
@@ -26,66 +27,67 @@ export class FirstComponentComponent implements OnInit {
 
 
   ngOnInit() {
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        this.gridCord[5 * i + j] = {x: i * 30, y: j * 30, obstacle: 0, f:null , g:null,h:null};
+    for (let i = 0; i < this.vGrid; i++) {
+      for (let j = 0; j < this.hGrid; j++) {
+        this.gridCord[this.hGrid * i + j] = {x: i * 30, y: j * 30, obstacle: 0};
 
       }
     }
+    console.log(this.height,this.width);
   }
 
   fillGrey(a: number, b: number,r: number): void {
     //console.log(a,b,r);  
-    let element = document.getElementsByTagName('rect')[Math.floor(a/6)+Math.floor(b/30)];
+    let element = document.getElementsByTagName('rect')[Math.floor(a/30)*this.hGrid+Math.floor(b/30)];
     if(this.mouseDown == true && !(element.style.fill == "green" || element.style.fill == "red" )){
       element.style.fill = "grey";
-      this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle = 1;
+      this.gridCord[Math.floor(a/30)*this.hGrid+Math.floor(b/30)].obstacle = 1;
       //console.log(this.gridCord[Math.floor(a/6)+Math.floor(b/30)].x);
     }
   }
 
   fillColor (a :number , b:number, r: number): void{
     //console.log('toggle');    
-    let element = document.getElementsByTagName('rect')[Math.floor(a/6)+Math.floor(b/30)];
+    let element = document.getElementsByTagName('rect')[Math.floor(a/30)*this.hGrid+Math.floor(b/30)];
     if(element.style.fill == "green"){  //clicked color is green
       //console.log('green');
       element.style.fill = "white";
       this.toFill = true;
-      this.color = 1;
+      this.color = 0;
     }else if (element.style.fill == "red") {//clicked color is red
       //console.log('red');
       element.style.fill = "white";
       this.toFill = true;
-      this.color = 0;
+      this.color = 1;
     }else{      //clicked color is white or grey
       //console.log ('white or grey');
 
         if(this.i == 0){      // prev click color red
-          element.style.fill = "red";
-          this.end = Math.floor(a/6)+Math.floor(b/30);
+          this.gridCord[Math.floor(a/30)*this.hGrid+Math.floor(b/30)].obstacle = 0;
+          element.style.fill = "green";
+          this.start = Math.floor(a/30)*this.hGrid+Math.floor(b/30);
           this.i++;
         }else if (this.i == 1) {// prev click color green
-          this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle = 0;
-          element.style.fill = "green";
-          this.start = Math.floor(a/6)+Math.floor(b/30);
-          this.i++;
+          element.style.fill = "red";
+          this.end = Math.floor(a/30)*this.hGrid+Math.floor(b/30);
+          this.i++;          
         }else{    // fill red green or grey
-          if(this.color == 0){
-            element.style.fill = "red";
-            this.end = Math.floor(a/6)+Math.floor(b/30);
-            this.color  = 2;
-          }else if(this.color == 1){
+          if(this.color == 0){  
             element.style.fill = "green";
-            this.start = Math.floor(a/6)+Math.floor(b/30);
-            this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle = 0;
+            this.start = Math.floor(a/30)*this.hGrid+Math.floor(b/30);
+            this.gridCord[Math.floor(a/30)*this.hGrid+Math.floor(b/30)].obstacle = 0;
             this.color = 2;
+          }else if(this.color == 1){
+            element.style.fill = "red";
+            this.end = Math.floor(a/30)*this.hGrid+Math.floor(b/30);
+            this.color  = 2;            
           }else{    // toggle grey
             if(element.style.fill == "grey"){
-              this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle = 0;
+              this.gridCord[Math.floor(a/30)*this.hGrid+Math.floor(b/30)].obstacle = 0;
               element.style.fill = "white";
             }else{
             element.style.fill = "grey";
-            this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle = 1;
+            this.gridCord[Math.floor(a/30)*this.hGrid+Math.floor(b/30)].obstacle = 1;
             //console.log(this.gridCord[Math.floor(a/6)+Math.floor(b/30)].obstacle);
           }
         }
@@ -102,23 +104,13 @@ export class FirstComponentComponent implements OnInit {
     this.mouseDown = true;
   }
 
-
-  //choose obstacle
-  chooseColor(a: number, b: number, r: number): void {
-    //console.log(a,b,r);
-    //console.log('hello');
-    let element = document.getElementsByTagName('rect')[Math.floor(a/6)+Math.floor(b/30)];
-    //window.alert("Hello There");
-    //let value:number = window.prompt("Please enter obstacle %");
-    //console.log(value);
-    //element.style.fill = "rgb(256, 0, 0,value/100)";
-  }
   //start q-learning search
   rl_reset(): void{
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        this.gridCord[5 * i + j] = {x: i * 30, y: j * 30, obstacle:0, f:null , g:null,h:null};
-
+    for (let i = 0; i < this.vGrid; i++) {
+      for (let j = 0; j < this.hGrid; j++) {
+        this.gridCord[this.hGrid * i + j].obstacle = 0;
+        let element = document.getElementsByTagName('rect')[this.hGrid * i + j];
+        element.style.fill = "white";
       }
     }
     this.i=0;
@@ -134,17 +126,18 @@ export class FirstComponentComponent implements OnInit {
     //var arr = this.direction_vector(this.start);
     //console.log(next);
     //console.log(arr);
+    this.reset_color();
     let visited: boolean[] = new Array();
     let distance: number[] = new Array();
     let parent: object;
     parent = {};
-    for(let j=0;j<25;j++){
-      visited[j]=(false);
+    for(let j=0;j<this.totalGrid;j++){
+      visited[j]=false;
     }
     var qu = new Array();
     visited[this.start]= true;
     distance[this.start] = 0;
-    qu.push(this.start);
+    qu.push(this.start);  
     while(qu.length != 0){
       var s =   qu[0];
       qu.shift();
@@ -163,10 +156,11 @@ export class FirstComponentComponent implements OnInit {
             }
             break;
           }
-        
+          if(visited[u]){
           let element = document.getElementsByTagName('rect')[u];
           // element.innerHTML. : ;
           element.style.fill = "yellow";
+          }
           parent[u] = s;
           qu.push(u);
         
@@ -195,16 +189,16 @@ export class FirstComponentComponent implements OnInit {
   direction_vector(a: number): number[]{
     var arr = new Array();
     
-    if(a+5 < 25 && this.gridCord[a+5].obstacle!=1){
-      arr.push(a+5);
+    if(a+this.hGrid < this.totalGrid && this.gridCord[a+this.hGrid].obstacle!=1){
+      arr.push(a+this.hGrid);
     }
-    if(a-5 >= 0 && this.gridCord[a-5].obstacle!=1){
-      arr.push(a-5);
+    if(a-this.hGrid >= 0 && this.gridCord[a-this.hGrid].obstacle!=1){
+      arr.push(a-this.hGrid);
     }
-    if((a+1)%5 !=0 && a+1 < 25 && this.gridCord[a+1].obstacle!=1){
+    if((a+1)%this.hGrid !=0 && a+1 < this.totalGrid && this.gridCord[a+1].obstacle!=1){
       arr.push(a+1);
     }
-    if((a)%5 !=0 && a-1>=0 && this.gridCord[a-1].obstacle!=1){
+    if((a)%this.hGrid !=0 && a-1>=0 && this.gridCord[a-1].obstacle!=1){
       arr.push(a-1);
     }
     return arr;
@@ -258,12 +252,9 @@ export class FirstComponentComponent implements OnInit {
           break;
       }
 
-      var neighbors = this.neighbors(currentNode);
+      var neighbors = this.direction_vector(currentNode);
 
       for (let u of neighbors){
-
-        console.log(this.distance(5,10));
-        this.update_FGH(f,g,h);
         if(closedList.includes(u)){
           continue;
         }
@@ -295,50 +286,27 @@ export class FirstComponentComponent implements OnInit {
   }  
 
   distance(a: number, b:number ): number {
+    a = Math.floor(a/30);
+    b = Math.floor(b/30);
     var x1 = Math.floor(a/this.hGrid);
     var y1 = a%this.vGrid;
     var x2 = Math.floor(b/this.hGrid);
     var y2 = b%this.vGrid;
     let dist = Math.abs(x1-x2) + Math.abs(y1-y2);
-    console.log(dist)
     return dist;
   }
 
-  neighbors(a : number) : number[]{
-    var arr = [];
-    
-    if(a+5 < 25 && this.gridCord[a+5].obstacle!=1){
-      arr.push(a+5);
-    }
-    if(a-5 >= 0 && this.gridCord[a-5].obstacle!=1){
-      arr.push(a-5);
-    }
-    if((a+1)%5 !=0 && a+1 < 25 && this.gridCord[a+1].obstacle!=1){
-      arr.push(a+1);
-    }
-    if((a)%5 !=0 && a-1>=0 && this.gridCord[a-1].obstacle!=1){
-      arr.push(a-1);
-    }
-    return arr;
-  }
   
-  update_FGH( f:Array<number> , g:Array<number> ,h:Array<number> ) :void{
-     for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        this.gridCord[i*5+j].f = f[i*5+j];
-        this.gridCord[i*5+j].g = g[i*5+j];
-        this.gridCord[i*5+j].h = h[i*5+j]; 
-      }
-    } 
-  }
 
   reset_color() :void{
    for (var u = this.totalGrid - 1; u >= 0; u--) {
-      if(u != this.start && u!= this.end && this.gridCord[u].obstacle != 1){
+      if(u != this.start && u!= this.end && this.gridCord[u].obstacle !=1){
         let element = document.getElementsByTagName('rect')[u];
         element.style.fill = "white";
+
       }
     }
+
   }
 
 }
